5990f561f8f44d1a66ced685ab8dbf4b2f7104f2
Jojo Rostock <jojorostock@yahoo.com>
2019-11-21 17:26:12 -0500

    Setup for intro scene

---
+++ Color Deficiency AR/Assets/FromIntro.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using UnityEngine.SceneManagement;
+
+public class FromIntro : MonoBehaviour
+{
+
+    public Button nextScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button toMain = nextScene.GetComponent<Button>();
+        toMain.onClick.AddListener(advanceScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void advanceScene()
+    {
+        SceneManager.LoadScene (sceneName:"MainScene");
+    }
+}
