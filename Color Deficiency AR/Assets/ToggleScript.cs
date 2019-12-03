461e2eac458168b212fd57e0c7037d3d53d0351c
Jojo Rostock <jojorostock@yahoo.com>
2019-12-03 14:47:26 -0500

    Added Font and fixed setup for a toggle

---
+++ Color Deficiency AR/Assets/ToggleScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleScript : MonoBehaviour
+{
+    public Button togg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button toggle = togg.GetComponent<Button>();
+		toggle.onClick.AddListener(toggleColor);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void toggleColor()
+    {
+        //TODO: have this switch on and off
+    }
+}
